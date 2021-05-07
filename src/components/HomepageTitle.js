58d9e689429c1a7dbf10b99b52079836d15f0770
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-05-07 03:51:32 +0300

    l

--- src/components/HomepageTitle.js
+++
@@ -1,9 +0,0 @@
-const HomepageTitle = () => {
-    return ( 
-        <div className="homepageTitle-container">
-        <h1 id="homepageTitle">ASTEROID_APP</h1>
-        </div>
-     );
-}
- 
-export default HomepageTitle;
